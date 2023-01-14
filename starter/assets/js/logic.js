04c4ad6348f7850612809dd1d1c16209f37a9dee
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-01-14 14:08:44 +0000

    added pseudocode and questions

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,16 @@
+// 1) Start timer
+    // function to start the timer when button is clicked 
+    // when the timer is pressed, the timer counts down from 75
+    // when the timer is pressed the quiz starts and questions appear 
+
+
+// 2) Start quiz
+   //  append the question choices 
+   // when the question is answered, it moves on to the next question
+   // when user selects the right answer - textContent = "Correct!"
+   // when use selects the wrong answer, textContext = "Wrong!" and 10 seconds is removed from the time left 
+   
+
+ // 3) Scores
+      // final score will keep track of how many answers were right 
+      
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,34 @@
+//put the quiz questions into an object array
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["booleans", "numbers", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        title: "The condition in a if/ else statement is enclosed within...:",
+        choices: ["curly brackets", "question marks", "parentheses", "dollar signs"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Arrays in Javascript can be used to store ...:",
+        choices: ["other arrays", "booleans", "all of the above", "numbers and strings"],
+        answer: "all of the above"
+    },
+
+    {
+        title: "String values must be enclosed within ... when being assigned to variables",
+        choices: ["pound signs", "curly brackets", "dollar signs", "quotes"],
+        answer: "quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is...",
+        choices: ["for loops", "HTML", "prompts", "console log"],
+        answer: "console log"
+    },
+
+];
