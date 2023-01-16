83eadb2ebcb8d740db1fbf0846652f69b62b9f29
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-01-16 19:45:17 +0000

    added function to check userChoice against correctAnswer, added code to turn the choices array into buttons

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,54 +1,57 @@
 // 1) Start timer
-    // function to start the timer when button is clicked 
-    // when the timer is pressed, the timer counts down from 75
-    // when the timer is pressed the quiz starts and questions appear 
+// function to start the timer when button is clicked 
+// when the timer is pressed, the timer counts down from 75
+// when the timer is pressed the quiz starts and questions appear 
 
 
 // 2) Start quiz
 // put questions in a unordered list
-   //  append the question choices to the page?
-   // for loop to go through the questions 
-   // when the question is answered, it moves on to the next question
-   // when user selects the right answer - textContent = "Correct!"
-   // when use selects the wrong answer, textContext = "Wrong!" and 10 seconds is removed from the time left 
-   
+//  append the question choices to the page?
+// for loop to go through the questions 
+// when the question is answered, it moves on to the next question
+// when user selects the right answer - textContent = "Correct!"
+// when use selects the wrong answer, textContext = "Wrong!" and 10 seconds is removed from the time left 
 
- // 3) Scores
-      // final score will keep track of how many answers were right 
-      
 
-      var questions = [
-        {
-            title: "Commonly used data types DO NOT include:",
-            choices: ["booleans", "numbers", "alerts", "numbers"],
-            answer: "alerts"
-        },
-    
-        {
-            title: "The condition in a if/ else statement is enclosed within...:",
-            choices: ["curly brackets", "question marks", "parentheses", "dollar signs"],
-            answer: "parentheses"
-        },
-    
-        {
-            title: "Arrays in Javascript can be used to store ...:",
-            choices: ["other arrays", "booleans", "all of the above", "numbers and strings"],
-            answer: "all of the above"
-        },
-    
-        {
-            title: "String values must be enclosed within ... when being assigned to variables",
-            choices: ["pound signs", "curly brackets", "dollar signs", "quotes"],
-            answer: "quotes"
-        },
-    
-        {
-            title: "A very useful tool used during development and debugging for printing content to the debugger is...",
-            choices: ["for loops", "HTML", "prompts", "console log"],
-            answer: "console log"
-        },
-    
-    ];
+// 3) Scores
+// final score will keep track of how many answers were right 
+var questionIndex = 0;
+var choicesEl = document.querySelector("#choices");
+var scores = 0;
+
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["booleans", "numbers", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        title: "The condition in a if/ else statement is enclosed within...:",
+        choices: ["curly brackets", "question marks", "parentheses", "dollar signs"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Arrays in Javascript can be used to store ...:",
+        choices: ["other arrays", "booleans", "all of the above", "numbers and strings"],
+        answer: "all of the above"
+    },
+
+    {
+        title: "String values must be enclosed within ... when being assigned to variables",
+        choices: ["pound signs", "curly brackets", "dollar signs", "quotes"],
+        answer: "quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is...",
+        choices: ["for loops", "HTML", "prompts", "console log"],
+        answer: "console log"
+    },
+
+];
 
 
 
@@ -69,12 +72,12 @@ var holdInterval = 0;
 var penalty = 10;
 
 
-   startTimer.addEventListener("click", function () {
+startTimer.addEventListener("click", function () {
     questionList.classList.remove('hide');
     startScreen.classList.add('hide');
-    loadQuestions();
+
     if (holdInterval === 0) {
-        holdInterval = setInterval(function() {
+        holdInterval = setInterval(function () {
             secondsLeft--;
             currentTime.textContent = "Time: " + secondsLeft;
 
@@ -83,28 +86,50 @@ var penalty = 10;
                 currentTime.textContent = "YOUR TIME IS UP, THE QUIZ HAS NOW FINISHED"
             }
         }, 1000);
-    } 
-    
-} ) ;
-
-var questionIndex = 0;
-var questionsDiv = document.querySelector("#questions")
+    }
+    loadQuestions();
+});
 
 
-function loadQuestions () {
-  var questionIndex = 0;
-  
 // I want questions to show up on the page, 
 // I want to create 4 buttons that have the answer content
 // I want to make a call to another function to see if the answer was right or wrong
 // take 10 seconds off time left if there is a wrong answer chosen
-var questionTitle = document.querySelector("#question-title");
-questionTitle.innerText = questions[0].title;
-var userChoices = document.querySelector("#choices")
-userChoices.innerText = questions[0].choices;
-console.log(questions[0].title);
-console.log(questions);
-console.log(questions[0].choices)
+// Page goes to next question after the button is clicked
+
+
+// function to check user choise against correct answer 
+// adds a point to the score variable if correct answer is chosen
+function checkAnswer(userChoice, correctAnswer) {
+    if (userChoice === correctAnswer) {
+        scores++;
+    }
+    else {
+        secondsLeft -= 10;
+    }
+    console.log(scores);
+}
+
+function loadQuestions() {
+
+    var questionTitle = document.querySelector("#question-title");
+    questionTitle.innerText = questions[questionIndex].title;
+
+// turns choices array into buttons
+    for (var i = 0; i < questions[questionIndex].choices.length; i++) {
+        const button = document.createElement('button');
+        button.innerText = questions[questionIndex].choices[i];
+        button.addEventListener('click', function (event) {
+            checkAnswer(event.target.textContent, questions[questionIndex].answer);
+
+            questionIndex++;
+            choicesEl.replaceChildren();
+            loadQuestions();
+        })
+        // apends buttons to the page 
+        choicesEl.appendChild(button);
+    }
+
 }
 
 
